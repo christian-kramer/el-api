254ab3be3cb2a4992f04769fc906186265682384
Christian Kramer <flyhi@prociate.com>
2018-05-17 03:44:19 -0500

    Added security dependency

---
+++ security.php
@@ -0,0 +1,25 @@
+<?php
+
+function sanitize($data)
+{
+    $data = trim($data);
+    $data = stripslashes($data);
+    $data = htmlspecialchars($data);
+    return $data;
+}
+
+function rglob($pattern, $flags = 0) {
+    $files = glob($pattern, $flags); 
+    foreach (glob(dirname($pattern).'/*', GLOB_ONLYDIR|GLOB_NOSORT) as $dir) {
+        $files = array_merge($files, rglob($dir.'/'.basename($pattern), $flags));
+    }
+    return $files;
+}
+
+function super_rmdir($dirname)
+{
+    array_map('unlink', glob("$dirname/*.*"));
+    rmdir($dirname);
+}
+
+?>
